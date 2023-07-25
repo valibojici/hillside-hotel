236be5ca5fc500fcd0a607df2e1c0ed806da9f87
Valentin Bojici <68808448+valibojici@users.noreply.github.com>
2023-07-25 17:56:00 +0300

    basic client that fetches and displays users from backend

---
+++ client/server.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.static('./public'));
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
