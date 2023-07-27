32f1f8300abc9cad72d5c424e359cf6f61973d9c
Valentin Bojici <68808448+valibojici@users.noreply.github.com>
2023-07-27 12:49:16 +0300

    use graphql

--- api/src/server.js
+++ api/src/server.js
@@ -1,12 +1,18 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const { createHandler } = require('graphql-http/lib/use/express');
 
 const { models } = require('./models');
+const { schema } = require('./graphql');
 
 const app = express();
 
 app.use(cors());
+
+app.use('/graphql', createHandler({
+    schema: schema,
+}))
 
 app.get('/', async (req, res) => {
     const users = await models.User.findAll()
