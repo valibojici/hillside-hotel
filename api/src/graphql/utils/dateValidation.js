c907fe3b073e034ebc3fe8ad2a874cfcab0cf1b2
Valentin Bojici <68808448+valibojici@users.noreply.github.com>
2023-07-31 13:22:46 +0300

    update

---
+++ api/src/graphql/utils/dateValidation.js
@@ -0,0 +1,25 @@
+const { GraphQLError } = require("graphql");
+const { DateTime } = require('luxon');
+
+const checkInOutValidation = (checkIn, checkOut) => {
+    checkIn = parseInt(checkIn);
+    checkOut = parseInt(checkOut);
+
+    if (isNaN(checkIn) || isNaN(checkOut)) {
+        throw new GraphQLError('Invalid Check In or Check Out date');
+    }
+    checkIn = DateTime.fromMillis(checkIn).startOf('day');
+    checkOut = DateTime.fromMillis(checkOut).startOf('day');
+
+    if (checkIn >= checkOut) {
+        throw new GraphQLError('Check In date must be before Check Out date');
+    }
+    const tommorow = DateTime.now().plus({ days: 1 }).startOf('day')
+
+    if (checkIn < tommorow) {
+        throw new GraphQLError('Check In date must be after today');
+    }
+    return { checkIn, checkOut };
+}
+
+module.exports = { checkInOutValidation };
