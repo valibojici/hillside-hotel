897b1bb4a6520b43511c56d2a91cbbd7dc29d78c
Valentin Bojici <68808448+valibojici@users.noreply.github.com>
2023-08-09 15:16:40 +0300

    add password validation message

--- api/src/graphql/mutations/user/signupMutation.js
+++ api/src/graphql/mutations/user/signupMutation.js
@@ -1,5 +1,6 @@
 const { GraphQLNonNull, GraphQLString, GraphQLError } = require("graphql");
 const { userType } = require("../../types/userType");
+const { ValidationError } = require("sequelize");
 
 module.exports = {
     type: userType,
@@ -13,19 +14,31 @@ module.exports = {
             throw new GraphQLError('Invalid token.');
         }
 
+        if (args.password.length < 8) {
+            throw new GraphQLError('Password must be at least 8 characters');
+        }
+
         // check if user has already signed up
         if ((await models.User.findOne({ where: { email: email } }))) {
             throw new GraphQLError('Already signed up.');
         }
 
         const hashedPassword = await models.User.hashPassword(args.password);
-        const user = await models.User.create({
-            username: args.username,
-            password: hashedPassword,
-            email: email,
-            role: 'user'
-        });
 
-        return user;
+        try {
+            const user = await models.User.create({
+                username: args.username,
+                password: hashedPassword,
+                email: email,
+                role: 'user'
+            });
+            return user;
+        } catch (error) {
+            if (error instanceof ValidationError) {
+                throw new GraphQLError(error.errors.map(e => e.message).join('.'));
+            }
+            throw error;
+        }
+
     }
 }
