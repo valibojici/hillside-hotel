36c067ce0f946226baf0c0e949ff2d86c480207c
Valentin Bojici <68808448+valibojici@users.noreply.github.com>
2023-08-17 19:51:37 +0300

    change cors

--- api/src/server.js
+++ api/src/server.js
@@ -25,7 +25,7 @@ const { formatError } = require('./graphql/utils/errors');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 
-app.use(cors({ origin: '*' }));
+app.use(cors());
 app.use(express.static(__dirname + '/public'));
 
 // stripe webhook
