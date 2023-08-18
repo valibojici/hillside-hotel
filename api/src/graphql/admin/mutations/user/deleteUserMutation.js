e036dacfd9714b577528be673b68c43da3184f3c
Valentin Bojici <68808448+valibojici@users.noreply.github.com>
2023-08-18 12:18:20 +0300

    check to not self-delete

--- api/src/graphql/admin/mutations/user/deleteUserMutation.js
+++ api/src/graphql/admin/mutations/user/deleteUserMutation.js
@@ -7,6 +7,9 @@ module.exports = {
         id: { type: new GraphQLNonNull(GraphQLID) },
     },
     resolve: async (parent, { id }, { jwtPayload, models }) => {
+        if (id == jwtPayload.data.userId) {
+            throw new GraphQLError('Cannot delete current user');
+        }
         const user = await models.User.findByPk(id);
         if (!user) {
             throw new GraphQLError('Invalid ID');
